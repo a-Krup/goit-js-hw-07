ba7b404475d8e8b63f8219d19215d74c0eee4357
Andrii KRUPKO <jobsdetekt.ak@gmail.com>
2025-01-18 11:04:15 +0200

    task six

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,41 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function createBoxes(amount) {
+  const boxesContainer = document.getElementById("boxes");
+
+  boxesContainer.innerHTML = "";
+
+  const elements = [];
+  for (let i = 0; i < amount; i++) {
+    const size = 30 + i * 10;
+    const div = document.createElement("div");
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    elements.push(div);
+  }
+
+  boxesContainer.append(...elements);
+}
+
+function destroyBoxes() {
+  const boxesContainer = document.getElementById("boxes");
+  boxesContainer.innerHTML = "";
+}
+
+document.querySelector("[data-create]").addEventListener("click", () => {
+  const input = document.querySelector("input");
+  const amount = parseInt(input.value, 10);
+
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+  }
+
+  input.value = "";
+});
+
+document
+  .querySelector("[data-destroy]")
+  .addEventListener("click", destroyBoxes);
